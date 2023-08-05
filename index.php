84b0c97b99a597770d41274555420db79cae1e2b
Jerry <126196933+Jerryluis10@users.noreply.github.com>
2023-08-05 01:15:56 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
+</head>
+<body>
+<div class="image-transition">
+  .    <nav>
+    <div class="wrapper">
+      <div class="logo"><a href="https://dinamixtech.com"><img src="img/mec_logo.png"></a></div>
+      <input type="radio" name="slider" id="menu-btn">
+      <input type="radio" name="slider" id="close-btn">
+     <div class="fl">
+      <ul class="nav-links">
+        <label for="close-btn" class="btn close-btn"><i class="fas fa-times"></i></label>
+          <!---<li>
+          <form class="nav-search">
+            <input class="nav-search-content" type="search" placeholder="Search..." aria-label="Search">
+            <button class="nav-search-btn" type="submit">Search</button>
+           </form>
+          </li>------>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Destination</a></li>
+          <li><a href="#">Tours</a></li>
+          <li><a href="#">Blog</a></li>
+          <li><a href="#">Contact</a></li>
+          <li>
+            <a href="#" class="desktop-item">Elements</i>
+              <span class="material-icons-sharp">expand_more</span>
+            </a>
+            <input type="checkbox" id="showMega">
+            <label for="showMega" class="mobile-item">Services<span class="material-icons-sharp">expand_more</span></label>
+            <div class="mega-box">
+              <div class="content">
+                <div class="row1">
+                  <img src="img/pexels-nathan-cowley-1153369.jpg" alt="">
+                </div>
+                <div class="row1">
+                  <header>CONTACT US</header>
+                  <ul class="mega-links">
+                    <li><a href="/contact_us/index.html">Telephone</a></li>
+                    <li><a href="/contact_us/index.html">Email</a></li>
+                    <li><a href="/contact_us/index.html">Inquiry Status</a></li>
+                    <li><a href="/contact_us/index.html">Customer Feedback</a></li>
+                    <li><a href="/contact_us/index.html">WhatsApp</a></li>
+                  </ul>
+                </div>
+                <div class="row1">
+                  <header>REPAIR SERVICE</header>
+                  <ul class="mega-links">
+                    <li><a href="">Request a Repair</a></li>
+                    <li><a href="#">Track a Repair</a></li>
+                    <li><a href="#">Find a Service Center</a></li>
+                    <li><a href="#">Warranty Information</a></li>
+                  </ul>
+                </div>
+                <div class="row1">
+                  <header>GUIDES</header>
+                  <ul class="mega-links">
+                    <li><a href="#"></a></li>
+                    <li><a href="teleport.html">Tips & How-to</a></li>
+                    <li><a href="hcr.html">Video Tutorials</a></li>
+                    <li><a href="split.html">Manuals</a></li>
+                    <li><a href="dat.html">Software & Firmware</a></li>
+                  </ul>
+                </div>
+              </div>  
+            </div>
+          </li>
+          <li><a href="#">Contact <span class="material-icons-sharp">expand_more</span></a></li>
+          <!--<button type="button" class="btn-subscribe">Subscribe</button>----->
+        </ul>
+         
+        <div class="serc">
+         <div>
+          <span class="material-icons-sharp">search</span>
+          <span class="material-icons-sharp">person</span>
+         </div>
+          <div><a href="">Quotation</a></div>
+        </div>
+     </div>
+      <label for="menu-btn" class="btn menu-btn"><i class="fas fa-bars"></i></label>
+    </div>
+</nav>
+
+<div class="feel">
+  <div class="fel"><h2>Embark on a Journey</h2></div>
+  <div class="expl">
+    <h1>Unveil the Beauty of Canada
+      Live, Work, Study
+    </h1>
+  </div>
+  <div class="get"><a href="">APPLY NOW  <span class="material-icons-sharp">arrow_right_alt</span></a></div>
+  <div class="lore">
+     <div class="splo">
+      <span class="material-icons-sharp">schedule</span>
+      <p>Seize the Moment: Travel the World, <br> One Memory at a Time</p>
+     </div>
+     <div class="splo">
+      <span class="material-icons-sharp">warning</span>
+      <p>Important Travel Advisory: Stay Informed <br> and Travel Safely With Us</p>
+     </div>
+  </div>
+</div>
+
+<section class="place">
+  <div class="show">
+    <div class="fst">
+     <h1></h1>
+     <p></p>
+    </div>
+    <div class="sec">
+      <div class="sec_fl">
+        <div class="seca">
+          <h1> </h1>
+          <p></p>
+        </div>
+        <div class="secb">
+          <h1></h1>
+          <p></p>
+        </div>
+      </div>
+      <div class="sec_und">
+        <h1></h1>
+        <p></p>
+      </div>
+
+    </div>
+  </div>
+</section>
+
+<section class="exp">
+<div class="exp_cont">
+  <div class="expes">
+
+  <h1>Packages We Offer</h1>
+  <p>Explore the world hassle-free with our comprehensive visa services. From tourist to family visas, we cater to your travel needs. Whether you're a student, professional, or a traveler, we've got you covered. Discover new horizons with ease and confidence with our visa solutions.</p>
+
+    <div class="trav">
+      <span class="material-icons-sharp">flight_takeoff</span>
+      <div class="trav_writ"> 
+        <div><h3>Your Path to Study Abroad</h3></div>
+      <div><p>Comprehensive packages for visiting, studying, and working abroad.</p></div>
+      </div>
+    </div>
+    <div class="trav">
+      <span class="material-icons-sharp">luggage</span>
+      <div>
+        <div class="trav_writ">
+          <div><h3>Tourist Visa Made Easy</h3></div>
+      <div><p> Hassle-free visa solutions for tourists, students, and workers.</p></div>
+        </div>
+      </div>
+    </div>
+    <div class="trav">
+      <span class="material-icons-sharp">payments</span>
+      <div> 
+      <div class="trav_writ">
+        <div><h3>Business Visa Simplified</h3></div>
+        <div><p>Discover new horizons with confidence through our visa services.</p></div>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="exp_img"></div>
+</div>
+</section>
+
+<section class="news_info">
+  <div class="news">
+    <div class="news_wrd">
+      <h1>Explore Mesmerizing Nature and Enchanting Cultures</h1>
+    </div>
+    <div class="news_des">
+      <p> Embark on an adventure through mesmerizing landscapes and captivating cultures. Unveil the world's hidden treasures with us. Discover, experience, and cherish unforgettable moments.</p>
+        <a href="">LEARN MORE</a>
+    </div>
+  </div>
+</section>
+
+<section class="social-media">
+  <h2>Be part of the discussion</h2>
+  <p>Stay updated! Connect with MEC NIGERIA for exclusive competitions,<br> fabulous prizes and latest news.</p>
+  <div class="social-links">
+    <a href=https://www.facebook.com/dinamixtech><i class="fab fa-facebook-f"></i></a>
+    <a href="#"><i class="fab fa-twitter"></i></a>
+    <a href="#"><i class="fab fa-instagram"></i></a>
+    <a href=https://www.linkedin.com/company/dinamix-tech><i class="fab fa-linkedin-in"></i></a>
+  </div>
+</section>
+
+<footer>
+  <div class="foot">
+    <div class="a_sec">
+      <div class="foot_img"><img src="img/mec_logo2.png" alt=""></div>
+      <div class="addres"><p>Badung, Bali — Jl. Desa Sawangan, No. 11 Nusa Dua, 81566 </p></div>
+      <div class="foot_soc">
+        <a href=https://www.facebook.com/dinamixtech><i class="fab fa-facebook-f"></i></a>
+        <a href="#"><i class="fab fa-twitter"></i></a>
+        <a href="#"><i class="fab fa-instagram"></i></a>
+        <a href=https://www.linkedin.com/company/dinamix-tech><i class="fab fa-linkedin-in"></i></a>
+      </div>
+    </div>
+    <div class="b_sec">
+      <div class="foot_hd"><h1>Menu</h1></div>
+      <div class="foot_wrsd">
+        <a>About Us</a>
+         <a> Services</a>
+         <a> FAQ</a>
+          <a>Contact Us</a>
+      </div>
+    </div>
+    <div class="c_sec">
+      <div class="c_sec_hd"><h1>Company</h1></div>
+      <div class="c_sec_wrd">
+        <a>Privacy Policy</a>
+          <a>Career</a>
+          <a>Term & Condition</a>
+      </div>
+    </div>
+    <div class="d_sec">
+      <div class="hd_foot"><h1>Our Newsletter</h1></div>
+      <div class="nws_let">
+        <p>Stay informed, subscribe to our newsletter for exciting updates, exclusive offers, and more!</p>
+      </div>
+      <form action="" method="post">
+      <?php  
+  // connect to database
+  
+  $conn = mysqli_connect("localhost", "root", "", "mec_nigeria");
+  
+  // check if connection is successful
+  
+  if(mysqli_connect_error()){
+     exit ('failed to connect: ' .mysqli_connect_error());
+  }
+
+$email = $_POST['email'];
+
+
+$query = "SELECT * FROM news_letter WHERE email = '$email'";
+
+// Execute the query
+$result = mysqli_query($conn, $query);
+
+// Check if the query was successful
+if (!$result) {
+    die('Query execution failed: ' . mysqli_error($conn));
+}
+
+
+?>
+
+
+
+    <h1 style="margin: auto; margin-top: 5%; text-align: center; font-size: 12px; color: white;">
+     <?php // Check if the user exists
+           $success = "Successful...";
+           if (mysqli_num_rows($result) > 0) {
+        
+   echo "Email already exist...";
+} else {
+    // Proceed with the desired action, such as inserting the user into the table
+    // or displaying a success message
+    echo  $success;
+
+    $sql = "INSERT INTO news_letter(email) 
+
+    VALUES('".$email."')";
+    
+   if ($conn->query($sql) === FALSE){
+        echo "error:" .$sql. ".<br>." .$conn->error;
+    }
+  
+    $query = "SET @num := 0";
+mysqli_query($conn, $query);
+
+$query = "UPDATE news_letter SET id = @num := (@num+1)";
+mysqli_query($conn, $query);
+
+$query = "ALTER TABLE news_letter AUTO_INCREMENT = 1";
+mysqli_query($conn, $query);
+  
+}?>
+<?php mysqli_close($conn); ?>
+        <div class="emal">
+          <input type="email" name="email" placeholder="Your Email Address">
+          <button type="submit">SIGN UP</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</footer>
+
+
+</div>
+</body>
+</html>
---
+++ news_letter.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 05, 2023 at 10:01 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mec_nigeria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news_letter`
+--
+
+CREATE TABLE `news_letter` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `date` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `news_letter`
+--
+
+INSERT INTO `news_letter` (`id`, `email`, `date`) VALUES
+(1, 'jerry@gmail.com', '2023-08-05 00:58:04');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `news_letter`
+--
+ALTER TABLE `news_letter`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `news_letter`
+--
+ALTER TABLE `news_letter`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
